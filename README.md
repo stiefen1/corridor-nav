04eb221f905ee53dad8eb44b9b443a70084a1424
Stephen Monnet <stephen-monnet@hotmail.com>
2025-11-03 14:40:35 +0100

    improve installation procedure

---
+++ README.md
@@ -0,0 +1,31 @@
+# Corridor Navigation
+
+## Installation
+
+## Clone this repository in recursive mode
+```bash
+git clone --recurse-submodules https://github.com/stiefen1/corridor-nav.git
+```
+
+## Conda
+Setup your own environment with python:
+```bash
+conda init
+conda env create -f env.yml 
+conda activate corridor-nav-env
+```
+
+## PSO
+Install PSO and its dependencies by running:
+```bash
+pip install -e submodules/pso
+```
+
+## SeaCharts
+Install SeaCharts by running:
+```bash
+pip install -e submodules/seacharts_ais
+```
+
+## Test
+Run ```examples/test_installation.py```
---
+++ examples/test_installation.py
@@ -0,0 +1,34 @@
+import os, numpy as np
+from seacharts.enc import ENC
+from pso import PSO, CostBase
+
+class QuadOpt(CostBase):
+    def eval(self, x:float, y:float, *args, **kwargs) -> float:
+        return np.min([(x+3)**2 + (y+3)**2, (x-3)**2 + (y-3)**2])
+
+# Create PSO optimizer
+optimizer = PSO(
+    cost=QuadOpt(),
+    lbx=(-10, -10),
+    ubx=(10, 10), # Search space limits
+    n_particles=50,
+    max_iter=300,
+    inertia=0.6,
+    c_cognitive=0.1,
+    c_social=0.3,
+    stop_at_variance=1e-6
+)
+
+# Run optimization
+optimizer.optimize()
+
+# Get PSO results
+best_position = optimizer.get_optimal_position()
+best_cost = optimizer.get_optimal_cost()
+print(f"Optimal particle: {best_position.tolist()}, Optimal cost: {best_cost:.2f}")
+
+# SeaCharts ENC
+config_path = os.path.join('src', 'corridor_opt', 'config', 'trondelag_1.yaml')
+enc = ENC(config_path)
+enc.display.start()
+enc.display.show()
