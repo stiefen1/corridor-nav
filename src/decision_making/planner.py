057ecad05e1a3358cb8201d16b53a0c60bab7f38
Aduragbemi Samuel Adetunji <aduragba@ntnu.no>
2025-11-14 00:55:28 +0100

    access historical weather data

--- src/decision_making/planner.py
+++ src/decision_making/planner.py
@@ -7,7 +7,8 @@ from corridor_opt.corridor import Corridor
 from corridor_opt.corridors_graph import GraphOfCorridors
 from power.energy_estimator import EnergyEstimator
 from power.total_force_estimator import ForceEstimator
-from weather.weather_helpers import WeatherSample, WeatherClient
+# from weather.weather_helpers import WeatherSample, WeatherClient
+from weather.weather_build_helpers import WeatherClient
 from traffic.ships import TargetShip
 from risk.model import RiskModel
 import datetime as dt, networkx as nx
@@ -24,14 +25,15 @@ class Planner:
         weather_client: Optional[WeatherClient] = None,
         ais_client: Optional[Any] = None,
         risk_model: Optional[RiskModel] = None,
-        mu: float = 1 # cost function weight: cost = risk + mu * energy_cons
+        mu: float = 1, # cost function weight: cost = risk + mu * energy_cons
+        weather_path: str = 'src/weather/data/kristiansund_weather.csv'
     ):
         self.graph_of_corridors = GraphOfCorridors(corridors)
         self.records = ais_client
         self.target_node = target_node
         self.energy_estimator = energy_estimator or EnergyEstimator()
         self.force_estimator = force_estimator or ForceEstimator()
-        self.weather_client = weather_client or WeatherClient(user_agent="ecdisAPP/1.0 ecdis@example.com", mode="met", grid_deg=0.01) # TODO: Replace with new version
+        self.weather_client = weather_client or WeatherClient(user_agent="Replay/1.0", mode="none", source="archive", archive_csv=weather_path) # mode = met, source = live for live data
         self.risk_model = risk_model or RiskModel()
         self.mu = mu
 
@@ -56,7 +58,7 @@ class Planner:
     def set_costs(
             self,
             u: float, # Surge speed
-            when_utc: dt.datetime, # current time
+            when_utc: dt.datetime, 
             ship_nominal_maneuverability: float, # Maneuverability without any external forces
             ship_nominal_tracking_accuracy: float, # Tracking accuracy without any external forces
             disable_wave: bool = False
@@ -84,7 +86,8 @@ class Planner:
                 travel_time = corridor.backbone.length / u # distance / speed
 
                 # Get weather sample at corridor's centroid
-                weather_sample = self.weather_client.get(when_utc, east=east, north=north) # TODO: Replace with new version
+                weather_sample = self.weather_client.get(when_utc, east=east, north=north)
+                print(weather_sample)
 
                 # Environmental forces
                 forces = self.force_estimator.get(u, psi, weather_sample, degrees=False, disable_wave=disable_wave)
